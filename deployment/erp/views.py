750847005f54ba2654ce0a8cc02d1cb178c29882
Balaji30589kl <163735560+Balaji30589kl@users.noreply.github.com>
2024-11-04 13:44:39 +0530

    Delete deployment directory

--- deployment/erp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-def home(request):
-    return HttpResponse("this is firstone")
-def base(request):
-    return render(request,'erp/base.html')
-
-
-# Create your views here.
